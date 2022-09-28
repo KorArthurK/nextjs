457e50996dc74f3000be57b3337618c3c0fa00db
Bill_Kim <kim0810@naver.com>
2022-09-29 01:20:01 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,27 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  },
+}
+
+// const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// const nextEnv = require("next-env");
+// const dotenvLoad = require("dotenv-load");
+
+// dotenvLoad();
+
+// const withEnv = nextEnv();
+
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     console.log("I'm in dev mode")
+//   }
+//   return withEnv(defaultConfig)
+// }
